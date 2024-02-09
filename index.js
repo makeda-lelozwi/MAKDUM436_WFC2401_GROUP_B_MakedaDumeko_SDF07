fa7f028d60b5ecb23db69d7c1a75d68569998bef
Makeda Lelozwi Dumeko <lelodumeko2@gmail.com>
2024-02-09 08:52:33 +0200

    add the shopping list to index.html

--- index.js
+++ index.js
@@ -11,11 +11,14 @@ const shoppingListInDB = ref(database, "shoppingList")
 
 const inputFieldEl = document.getElementById("input-field")
 const addButtonEl = document.getElementById("add-button")
+const shoppingListEl = document.getElementById("shopping-list")
 
 addButtonEl.addEventListener("click", function() {
   let inputValue = inputFieldEl.value
 
   push(shoppingListInDB, inputValue)
 
-  console.log(inputValue)
+ shoppingListEl.innerHTML += `
+  <li>${inputValue}</li>
+ `
 })
